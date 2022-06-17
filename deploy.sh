5b5d9717b3a6d001d5fd194372072ea25deb287b
chloexu <xuzhuqing1124@gmail.com>
2022-06-17 18:01:49 -0400

    edit deploy shell script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ export PATH_TO_CONFIG=./config/prod.json
 
 GOARCH=amd64 GOOS=linux go build .
 
-zip package.zip case-tracker
+zip package.zip case-tracker config/*
 
 echo "Now you can upload package.zip to your Lambda function!"
 
